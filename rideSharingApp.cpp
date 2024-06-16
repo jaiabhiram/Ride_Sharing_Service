76a0016054492ab8cac1043e4360b65b2765774a
jaiabhiram <jaiabhirambellamkonda558@gmail.com>
2024-06-16 18:15:29 +0530

    Added Comments

--- rideSharingApp.cpp
+++ rideSharingApp.cpp
@@ -9,19 +9,30 @@ using namespace std;
 
 int main()
 {
+    // Creating Users.
     Traveler* traveler1 = new Traveler("Abhiram", "8919578343", "AP5588");
     TravelerCompanion* tc1 = new TravelerCompanion("Alpha", "XYZ");
 
+    // creating a TripManager (System).
     TripManager* tripManager = TripManager::getTripManagerInstance();
 
+    // Traveler Interacting with System to create a Trip.
     string tripId = tripManager->createTrip(traveler1, new Location(5,5), new Location(10,10));
 
+    // TravelerCompanion Interacting with System to get all the Available Trips.
     vector<string> trips = tripManager->getAvailableTrips();
+
+    // TravelerCompanion choose one of the available Trips. 
     tripManager->addTravelerCompanion(trips[0], tc1);
 
+    // Traveler choose to strat the trip.
     tripManager->startTrip(trips[0]);
+
+    // Traveler giving updates to participates of the trip.
     tripManager->notifyUpdatesToTravelerCompanions(trips[0], "Almost reached to destination");
+
+    // Traveler completed the trip.
     tripManager->completeTrip(trips[0]);
-    
+
     return(0);
 }
